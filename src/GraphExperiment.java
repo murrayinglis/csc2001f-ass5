0e467a82a40dcf1334fac9a359a98f1974ed6ad7
murrayinglis <murrayinglis101@gmail.com>
2023-04-22 13:18:14 +0200

    Added RandomGraphGenerator class

--- src/GraphExperiment.java
+++ src/GraphExperiment.java
@@ -1,16 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class GraphExperiment {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // Generating random graph
+        RandomGraphGenerator randomGraph = new RandomGraphGenerator(9, 5);
+        randomGraph.generateGraph();
+
+        // Generating graph file
+        try (PrintWriter out = new PrintWriter("Graph.txt")) {
+            out.println(randomGraph.toString());
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
 
         // This is sample code from Vula
         Graph g = new Graph( );
         try
         {   	
-            FileReader fin = new FileReader("Graph1.txt");
+            FileReader fin = new FileReader("Graph.txt");
             Scanner graphFile = new Scanner( fin );
 
             // Read the edges and insert
@@ -43,7 +59,9 @@ public class GraphExperiment {
          System.out.println( g.getVertexMap().size( ) + " vertices" );
 
         // My own code from here
-        g.dijkstra("A");
-        g.printPath("G");
+        g.dijkstra("Node1");
+        g.printPath(randomGraph.getLastVertex());
+
+        
     }    
 }
